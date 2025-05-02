56ecf98d73c2ec0ab30670e7affdc03648598852
Moses225 <moisestephanekouassi@gmail.com>
2025-05-02 03:18:04 -0500

    Fix route path syntax for Heroku deployment

--- backend/server.js
+++ backend/server.js
@@ -33,9 +33,10 @@ app.use(errorHandler);
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../frontend/build')));
 
-  app.get('/*', function (req, res) {
+  app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
   });
+  
 }
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
